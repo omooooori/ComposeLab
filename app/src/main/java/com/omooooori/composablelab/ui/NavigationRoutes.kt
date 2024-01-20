ddefd29fa8097292cf48fe4e09ad7d7c17857297
omooooori <omooooori432@gmail.com>
2024-01-20 12:32:36 +0900

    Create list screen and enable to transition from MainScreen to created screen.

---
+++ app/src/main/java/com/omooooori/composablelab/ui/NavigationRoutes.kt
@@ -0,0 +1,6 @@
+package com.omooooori.composablelab.ui
+
+object NavigationRoutes {
+    const val MAIN_SCREEN = "mainScreen"
+    const val LEARNING_ENGLISH_UI = "learningEnglishUI"
+}
---
+++ app/src/main/java/com/omooooori/composablelab/ui/composable/MainScreen.kt
@@ -0,0 +1,75 @@
+package com.omooooori.composablelab.ui.composable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.omooooori.composablelab.ui.NavigationRoutes
+
+@Composable
+fun MainScreen(
+    navController: NavController
+) {
+    val navigateToScreen: (String) -> Unit = { route ->
+        navController.navigate(route)
+    }
+
+    val screenList = hashMapOf(
+        "Learning English UI" to NavigationRoutes.LEARNING_ENGLISH_UI
+    )
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        items(screenList.entries.toList()) { (title, route) ->
+            ListItem(
+                title = title,
+                onClick = { navigateToScreen(route) }
+            )
+        }
+    }
+}
+
+@Composable
+fun ListItem(
+    title: String,
+    onClick: () -> Unit
+) {
+    Box(modifier = Modifier
+        .padding(horizontal = 24.dp, vertical = 8.dp)
+        .fillMaxWidth()
+        .background(color = Color.Cyan, shape = RoundedCornerShape(8.dp))
+        .border(1.dp, Color.Black, shape = RoundedCornerShape(8.dp))
+        .clickable { onClick() }
+    ) {
+        Text(
+            text = title,
+            style = TextStyle(fontSize = 20.sp),
+            modifier = Modifier
+                .padding(horizontal = 12.dp, vertical = 4.dp)
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen(navController = rememberNavController())
+}
