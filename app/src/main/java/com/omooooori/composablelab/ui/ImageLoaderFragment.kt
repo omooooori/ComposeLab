6d228cac5822b42c8ff5c8dff1edfbca38913891
omooooori <omooooori432@gmail.com>
2024-04-09 21:48:31 +0900

    Change image.

--- app/src/main/java/com/omooooori/composablelab/ui/ImageLoaderFragment.kt
+++ app/src/main/java/com/omooooori/composablelab/ui/ImageLoaderFragment.kt
@@ -1,18 +1,29 @@
 package com.omooooori.composablelab.ui
 
+import android.graphics.drawable.Animatable
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import androidx.fragment.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import coil.load
+import coil.ImageLoader
+import coil.request.ImageRequest
 import com.bumptech.glide.Glide
-import com.omooooori.composablelab.R
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.facebook.drawee.controller.BaseControllerListener
+import com.facebook.imagepipeline.image.ImageInfo
 import com.omooooori.composablelab.databinding.FragmentImageLoaderBinding
 import com.omooooori.composablelab.ui.viewmodel.ImageLoaderViewModel
+import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 import javax.inject.Inject
 
 
@@ -42,23 +53,79 @@ class ImageLoaderFragment : Fragment() {
     }
 
     private fun loadGlideImage() {
+        val startTime = System.currentTimeMillis()
         Glide.with(requireContext())
             .load(viewModel.imageUrl)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                    val endTime = System.currentTimeMillis()
+                    val duration = endTime - startTime
+                    binding.glidePerformanceLabel.text = "Glide: ${duration}ms"
+                    return false
+                }
+            })
             .into(binding.glideImageView)
     }
 
     private fun loadFrescoImage() {
-        binding.frescoImageView.setImageURI(Uri.parse(viewModel.imageUrl))
+        val startTime = System.currentTimeMillis()
+        val listener = object : BaseControllerListener<ImageInfo>() {
+            override fun onFinalImageSet(id: String?, imageInfo: ImageInfo?, animatable: Animatable?) {
+                val endTime = System.currentTimeMillis()
+                val duration = endTime - startTime
+                binding.frescoPerformanceLabel.text = "Fresco: ${duration}ms"
+            }
+
+            override fun onFailure(id: String?, throwable: Throwable?) {
+                Timber.e("Failed to load image using fresco. id: $id, throwable: ${throwable.toString()}")
+            }
+        }
+
+        val controller = Fresco.newDraweeControllerBuilder()
+            .setUri(Uri.parse(viewModel.imageUrl))
+            .setControllerListener(listener)
+            .build()
+        binding.frescoImageView.controller = controller
     }
 
     private fun loadCoilImage() {
-        binding.coilImageView.load(viewModel.imageUrl)
+        val startTime = System.currentTimeMillis()
+        val request = ImageRequest.Builder(requireContext())
+            .data(viewModel.imageUrl)
+            .target(binding.coilImageView)
+            .listener(
+                onSuccess = { _, _ ->
+                    val endTime = System.currentTimeMillis()
+                    val duration = endTime - startTime
+                    binding.coilPerformanceLabel.text = "Coil: ${duration}ms"
+                },
+                onError = { _, _ ->
+                    Timber.e("Failed to load image using coil.")
+                }
+            )
+            .build()
+        ImageLoader(requireContext()).enqueue(request)
     }
 
     private fun loadPicassoImage() {
+        val startTime = System.currentTimeMillis()
         Picasso.get()
             .load(viewModel.imageUrl)
-            .into(binding.picassoImageView)
+            .into(binding.picassoImageView, object : Callback {
+                override fun onSuccess() {
+                    val endTime = System.currentTimeMillis()
+                    val duration = endTime - startTime
+                    binding.picassoPerformanceLabel.text = "Picasso: ${duration}ms"
+                }
+
+                override fun onError(e: Exception?) {
+                    Timber.e("Failed to load image using picasso. e: ${e.toString()}")
+                }
+            })
     }
 
     companion object {
--- app/src/main/java/com/omooooori/composablelab/ui/viewmodel/ImageLoaderViewModel.kt
+++ app/src/main/java/com/omooooori/composablelab/ui/viewmodel/ImageLoaderViewModel.kt
@@ -5,6 +5,6 @@ import javax.inject.Inject
 
 class ImageLoaderViewModel @Inject constructor() : ViewModel() {
 
-    val imageUrl = "https://placekitten.com/200/300"
+    val imageUrl = "https://images.unsplash.com/photo-1584395630827-860eee694d7b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
 
 }
