be67d6a52daf300d427ba5f2d444a14046755781
omooooori <omooooori432@gmail.com>
2024-06-01 18:54:39 +0900

    Add Expand View.

---
+++ app/src/main/java/com/omooooori/composablelab/ui/composable/expandview/ExpandView.kt
@@ -0,0 +1,56 @@
+package com.omooooori.composablelab.ui.composable.expandview
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+@Composable
+fun ExpandView() {
+    var expanded by remember { mutableStateOf(false) }
+    Box(
+        modifier = Modifier
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(
+                        Color(0xFFF3F6FF),
+                        Color(0xFFA7BEFF)
+                    ),
+                    start = Offset(0f, 0f),
+                    end = Offset(1f, 1f)
+                ),
+                shape = RoundedCornerShape(12.dp)
+            )
+            .animateContentSize()
+            .width(if (expanded) 12.dp else 178.dp)
+            .height(155.dp)
+            .fillMaxWidth()
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null
+            ) {
+                expanded = !expanded
+            }
+    )
+}
+
+@Preview
+@Composable
+fun ExpandViewPreview() {
+    ExpandView()
+}
